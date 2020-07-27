0b96a4749a0ff0ffa9f12c50df4b980dfb454a88
konradmayer <konrad.mayer@boku.ac.at>
2020-07-27 08:37:22 +0200

    Add viridis default for plotmap

---
+++ R/plotmap.R
@@ -0,0 +1,11 @@
+#' Plotmap using viridis
+#'
+#' @description Use the viridis colorramp in plotmap as default. The model can
+#'   contain the special column name .wavelength to specify the wavelength axis.
+#'
+#' @inheritParams hyperSpec::plotmap
+#' @return
+#' @export
+plotmap_viridis <- function(object, model = spc ~ x * y, func = mean, func.args = list(), ...) {
+  hyperSpec::plotmap(object, model = model, func = func, func.args = func.args, par.settings = rasterVis::viridisTheme(), ...)
+}
