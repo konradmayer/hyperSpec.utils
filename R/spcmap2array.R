d30c3e0d5cc0ae9e2539067b6db20c1e0d1bb96c
konradmayer <konrad.mayer@boku.ac.at>
2020-07-27 15:23:48 +0200

    add sorting to spcmap2array and allow dimnames

--- R/spcmap2array.R
+++ R/spcmap2array.R
@@ -2,13 +2,29 @@
 #'
 #' @description Convert spatially resolved object of class hyperSpec to an array.
 #' @param x A \code{hyperSpec} object with variables "x" and "y" in \code{@data}.
+#' @param set_dimnames logical; specify whether values for x, y and wavelength should be used as dimnames.
 #' @return An array with the dimensions "x" "y" and "nwl", with the wavelength vector attached as an attribute.
 #' @export
 #'
-spcmap2array <- function(x) {
+spcmap2array <- function(x, set_dimnames = FALSE) {
+  # input validation
   stopifnot(is.hyperSpecMap(x))
+
+  # sort dimensions
+  x <- x[order(x$x), ]
+  x <- x[order(x$y), ]
+  x <- hyperSpec::orderwl(x)
+
+  # create array
   dimensions <- spcmap_dim(x)
   out <- array(as.vector(x[[]]), dim = dimensions)
   attr(out, "wavelength") <- x@wavelength
+  if (set_dimnames) {
+    dimnames(out) <- list(
+      x = unique(x$x), y = unique(x$y),
+      nwl = x@wavelength
+    )
+  }
+
   out
 }
